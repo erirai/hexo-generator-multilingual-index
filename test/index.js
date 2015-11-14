1f5644281c88ce53aace9be6e7418624308edd94
Adrian Haasler García <dev@adrianhaasler.com>
2015-11-14 18:59:46 +0000

    Add redirection page
    The base URL will redirect to the default language, which is the first
    in the array of languages.

--- test/index.js
+++ test/index.js
@@ -1,9 +1,24 @@
 'use strict';
 
 var should = require('chai').should(); // eslint-disable-line
-var fs = require('hexo-fs');
+var hexoFs = require('hexo-fs');
+var nunjucks = require('nunjucks');
+var env = new nunjucks.Environment();
+var fs = require('fs');
 var pathFn = require('path');
 var Hexo = require('hexo');
+
+nunjucks.configure({
+  autoescape: false,
+  watch: false
+});
+
+env.addFilter('uriencode', function(str) {
+  return encodeURI(str);
+});
+
+var tmplSrc = pathFn.join(__dirname, '../redirect.html');
+var tmpl = nunjucks.compile(fs.readFileSync(tmplSrc, 'utf8'), env);
 
 describe('Index generator', function() {
   var hexo = new Hexo(__dirname, {silent: true});
@@ -30,7 +45,7 @@ describe('Index generator', function() {
   }
 
   before(function() {
-    return fs.mkdirs(baseDir).then(function() {
+    return hexoFs.mkdirs(baseDir).then(function() {
       hexo.init();
       process(newFile({
         path: 'config_en.yml',
@@ -46,7 +61,7 @@ describe('Index generator', function() {
   });
 
   after(function() {
-    return fs.rmdir(baseDir);
+    return hexoFs.rmdir(baseDir);
   });
 
   // Default config
@@ -87,52 +102,55 @@ describe('Index generator', function() {
 
     var result = generator(locals);
 
-    result.length.should.eql(4);
+    result.length.should.eql(5);
 
-    for (var i = 0, len = result.length; i < len; i++) {
+    // Redirection
+    result[0].path.should.eql('');
+
+    for (var i = 1, len = result.length; i < len; i++) {
       result[i].layout.should.eql(['index', 'archive']);
       result[i].data.total.should.eql(2);
     }
 
-    result[0].path.should.eql('en/');
-    result[0].data.current_url.should.eql('en/');
-    result[0].data.posts.should.eql(enPosts.limit(2));
-    result[0].data.prev.should.eql(0);
-    result[0].data.prev_link.should.eql('');
-    result[0].data.next.should.eql(2);
-    result[0].data.next_link.should.eql('en/page/2/');
-    result[0].data.__index.should.be.true;
-    result[0].data.lang.should.eql('en');
-
-    result[1].path.should.eql('en/page/2/');
-    result[1].data.current_url.should.eql('en/page/2/');
-    result[1].data.posts.should.eql(enPosts.skip(2));
-    result[1].data.prev.should.eql(1);
-    result[1].data.prev_link.should.eql('en/');
-    result[1].data.next.should.eql(0);
-    result[1].data.next_link.should.eql('');
+    result[1].path.should.eql('en/');
+    result[1].data.current_url.should.eql('en/');
+    result[1].data.posts.should.eql(enPosts.limit(2));
+    result[1].data.prev.should.eql(0);
+    result[1].data.prev_link.should.eql('');
+    result[1].data.next.should.eql(2);
+    result[1].data.next_link.should.eql('en/page/2/');
     result[1].data.__index.should.be.true;
     result[1].data.lang.should.eql('en');
 
-    result[2].path.should.eql('es/');
-    result[2].data.current_url.should.eql('es/');
-    result[2].data.posts.should.eql(esPosts.limit(2));
-    result[2].data.prev.should.eql(0);
-    result[2].data.prev_link.should.eql('');
-    result[2].data.next.should.eql(2);
-    result[2].data.next_link.should.eql('es/pagina/2/');
+    result[2].path.should.eql('en/page/2/');
+    result[2].data.current_url.should.eql('en/page/2/');
+    result[2].data.posts.should.eql(enPosts.skip(2));
+    result[2].data.prev.should.eql(1);
+    result[2].data.prev_link.should.eql('en/');
+    result[2].data.next.should.eql(0);
+    result[2].data.next_link.should.eql('');
     result[2].data.__index.should.be.true;
-    result[2].data.lang.should.eql('es');
+    result[2].data.lang.should.eql('en');
 
-    result[3].path.should.eql('es/pagina/2/');
-    result[3].data.current_url.should.eql('es/pagina/2/');
-    result[3].data.posts.should.eql(esPosts.skip(2));
-    result[3].data.prev.should.eql(1);
-    result[3].data.prev_link.should.eql('es/');
-    result[3].data.next.should.eql(0);
-    result[3].data.next_link.should.eql('');
+    result[3].path.should.eql('es/');
+    result[3].data.current_url.should.eql('es/');
+    result[3].data.posts.should.eql(esPosts.limit(2));
+    result[3].data.prev.should.eql(0);
+    result[3].data.prev_link.should.eql('');
+    result[3].data.next.should.eql(2);
+    result[3].data.next_link.should.eql('es/pagina/2/');
     result[3].data.__index.should.be.true;
     result[3].data.lang.should.eql('es');
+
+    result[4].path.should.eql('es/pagina/2/');
+    result[4].data.current_url.should.eql('es/pagina/2/');
+    result[4].data.posts.should.eql(esPosts.skip(2));
+    result[4].data.prev.should.eql(1);
+    result[4].data.prev_link.should.eql('es/');
+    result[4].data.next.should.eql(0);
+    result[4].data.next_link.should.eql('');
+    result[4].data.__index.should.be.true;
+    result[4].data.lang.should.eql('es');
 
     // Restore config
     hexo.config.index_generator.per_page = 10;
@@ -143,35 +161,37 @@ describe('Index generator', function() {
 
     var result = generator(locals);
 
-    result.length.should.eql(2);
+    result.length.should.eql(3);
 
-    result[0].path.should.eql('en/');
-    result[0].layout.should.eql(['index', 'archive']);
-    result[0].data.base.should.eql('en/');
-    result[0].data.total.should.eql(1);
-    result[0].data.current.should.eql(1);
-    result[0].data.current_url.should.eql('en/');
-    result[0].data.posts.should.eql(enPosts);
-    result[0].data.prev.should.eql(0);
-    result[0].data.prev_link.should.eql('');
-    result[0].data.next.should.eql(0);
-    result[0].data.next_link.should.eql('');
-    result[0].data.__index.should.be.true;
-    result[0].data.lang.should.eql('en');
+    result[0].path.should.eql('');
 
-    result[1].path.should.eql('es/');
+    result[1].path.should.eql('en/');
     result[1].layout.should.eql(['index', 'archive']);
-    result[1].data.base.should.eql('es/');
+    result[1].data.base.should.eql('en/');
     result[1].data.total.should.eql(1);
     result[1].data.current.should.eql(1);
-    result[1].data.current_url.should.eql('es/');
-    result[1].data.posts.should.eql(esPosts);
+    result[1].data.current_url.should.eql('en/');
+    result[1].data.posts.should.eql(enPosts);
     result[1].data.prev.should.eql(0);
     result[1].data.prev_link.should.eql('');
     result[1].data.next.should.eql(0);
     result[1].data.next_link.should.eql('');
     result[1].data.__index.should.be.true;
-    result[1].data.lang.should.eql('es');
+    result[1].data.lang.should.eql('en');
+
+    result[2].path.should.eql('es/');
+    result[2].layout.should.eql(['index', 'archive']);
+    result[2].data.base.should.eql('es/');
+    result[2].data.total.should.eql(1);
+    result[2].data.current.should.eql(1);
+    result[2].data.current_url.should.eql('es/');
+    result[2].data.posts.should.eql(esPosts);
+    result[2].data.prev.should.eql(0);
+    result[2].data.prev_link.should.eql('');
+    result[2].data.next.should.eql(0);
+    result[2].data.next_link.should.eql('');
+    result[2].data.__index.should.be.true;
+    result[2].data.lang.should.eql('es');
 
     // Restore config
     hexo.config.index_generator.per_page = 10;
@@ -181,8 +201,8 @@ describe('Index generator', function() {
     it('default order', function() {
       var result = generator(locals);
 
-      result[0].data.posts.should.eql(enPosts);
-      result[1].data.posts.should.eql(esPosts);
+      result[1].data.posts.should.eql(enPosts);
+      result[2].data.posts.should.eql(esPosts);
     });
 
     it('custom order', function() {
@@ -190,25 +210,25 @@ describe('Index generator', function() {
 
       var result = generator(locals);
 
-      result[0].data.posts.eq(0).source.should.eql('two');
-      result[0].data.posts.eq(1).source.should.eql('three');
-      result[0].data.posts.eq(2).source.should.eql('one');
+      result[1].data.posts.eq(0).source.should.eql('two');
+      result[1].data.posts.eq(1).source.should.eql('three');
+      result[1].data.posts.eq(2).source.should.eql('one');
 
-      result[1].data.posts.eq(0).source.should.eql('dos');
-      result[1].data.posts.eq(1).source.should.eql('tres');
-      result[1].data.posts.eq(2).source.should.eql('uno');
+      result[2].data.posts.eq(0).source.should.eql('dos');
+      result[2].data.posts.eq(1).source.should.eql('tres');
+      result[2].data.posts.eq(2).source.should.eql('uno');
 
       hexo.config.index_generator.order_by = 'order';
 
       result = generator(locals);
 
-      result[0].data.posts.eq(0).source.should.eql('one');
-      result[0].data.posts.eq(1).source.should.eql('three');
-      result[0].data.posts.eq(2).source.should.eql('two');
+      result[1].data.posts.eq(0).source.should.eql('one');
+      result[1].data.posts.eq(1).source.should.eql('three');
+      result[1].data.posts.eq(2).source.should.eql('two');
 
-      result[1].data.posts.eq(0).source.should.eql('uno');
-      result[1].data.posts.eq(1).source.should.eql('tres');
-      result[1].data.posts.eq(2).source.should.eql('dos');
+      result[2].data.posts.eq(0).source.should.eql('uno');
+      result[2].data.posts.eq(1).source.should.eql('tres');
+      result[2].data.posts.eq(2).source.should.eql('dos');
 
       // Restore config
       hexo.config.index_generator.order_by = '-date';
@@ -219,16 +239,45 @@ describe('Index generator', function() {
 
       var result = generator(locals);
 
-      result[0].data.posts.eq(0).source.should.eql('one');
-      result[0].data.posts.eq(1).source.should.eql('two');
-      result[0].data.posts.eq(2).source.should.eql('three');
+      result[1].data.posts.eq(0).source.should.eql('one');
+      result[1].data.posts.eq(1).source.should.eql('two');
+      result[1].data.posts.eq(2).source.should.eql('three');
 
-      result[1].data.posts.eq(0).source.should.eql('uno');
-      result[1].data.posts.eq(1).source.should.eql('dos');
-      result[1].data.posts.eq(2).source.should.eql('tres');
+      result[2].data.posts.eq(0).source.should.eql('uno');
+      result[2].data.posts.eq(1).source.should.eql('dos');
+      result[2].data.posts.eq(2).source.should.eql('tres');
 
       // Restore config
       hexo.config.index_generator.order_by = '-date';
+    });
+  });
+
+  describe('redirection', function() {
+    it('english', function() {
+      var result = generator(locals);
+
+      // Redirection
+      result[0].path.should.eql('');
+      result[0].data.should.eql(tmpl.render({
+        config: hexo.config,
+        redirect: 'en/'
+      }));
+    });
+
+    it('español', function() {
+      hexo.config.language = ['es', 'en', 'default'];
+
+      var result = generator(locals);
+
+      // Redirection
+      result[0].path.should.eql('');
+      result[0].data.should.eql(tmpl.render({
+        config: hexo.config,
+        redirect: 'es/'
+      }));
+
+      // Restore config
+      hexo.config.language = ['en', 'es', 'default'];
     });
   });
 });
